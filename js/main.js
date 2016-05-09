8caffdf4657bf95592dd63119a25e845dca51218
meman <dancamina@mit-3-14.acm.ac.uk>
2016-05-09 15:19:03 +0100

    edit slide to allow for normal behavior

--- js/main.js
+++ js/main.js
@@ -1,40 +1,33 @@
 'use strict';
 
 var colorEditor = (function () {
-  var selectedColor = [0,0,0,1],
-      isAlphaEditing = false;
+    var selectedColor = [0, 0, 0, 1], isAlphaEditing = false;
+    //  Cached Dom
+    var $container = $('.container'), $colorEditor = $('.color-editor'), $alphaSlider = $colorEditor.find('.alpha-slider');
+    //Bind Events
+    $alphaSlider.find('i').mousedown(setAlphaEditing);
+    $container.mousemove(function(event){if(isAlphaEditing){setAlpha(event);}})
+        .mouseup(function () {if(isAlphaEditing){setAlphaEditing(event);}});
 
-  //  Cached Dom
-  var $colorEditor = $('.color-editor'),
-      $alphaSlider = $colorEditor.find('.alpha-slider');
+    _render();
 
-  //Bind Events
-
-  $alphaSlider.find('i')
-      .mousedown(setAlphaEditing)
-      .mouseup(setAlphaEditing);
-
-  $alphaSlider
-      .mouseleave(setAlphaEditing)
-      .mousemove(setAlpha);
-
-  _render();
-  function _render() {
-      $colorEditor.find('.color-block').css('background-color','rgba('+selectedColor.join(',')+')');
-      $alphaSlider.css('background-color','rgb('+selectedColor.slice(0,3).join(',')+')');
-      $alphaSlider.find('i').css('left',selectedColor[3]*100+'%');
-  }
-  function setAlphaEditing(event){
-      isAlphaEditing = (typeof event === 'boolean') ? event : !(event.type === 'mouseup' || event.type === 'mouseleave');
-  }
-  function setAlpha(event){
-    if(isAlphaEditing){
-      var $this = $(this),
-          slideValue = +(((event.pageX - $this.offset().left)/parseInt($this.css('width'))).toFixed(2));
-      slideValue = slideValue >= 1 ? 1 : slideValue <= 0 ? 0 :slideValue;
-      selectedColor[3] = slideValue;
-      _render();
+    function _render() {
+        $colorEditor.find('.color-block').css('background-color', 'rgba(' + selectedColor.join(',') + ')');
+        $alphaSlider.css('background-color', 'rgb(' + selectedColor.slice(0, 3).join(',') + ')');
+        $alphaSlider.find('i').css('left', selectedColor[3] * 100 + '%');
     }
-  }
+    function setAlphaEditing(event) {
+        isAlphaEditing = (typeof event === 'boolean') ? event : !(event.type === 'mouseup' || event.type === 'mouseleave');
+        $container.toggleClass('isDragging', isAlphaEditing);
+        console.log(isAlphaEditing);
+    }
+    function setAlpha(event) {
+        if (isAlphaEditing) {
+            var val = +((event.pageX - $alphaSlider.offset().left ) / parseInt($alphaSlider.css('width')).toFixed(2));
+            val = val >= 1 ? 1 : val <= 0 ? 0 : val;
+            selectedColor[3] = val;
+            _render();
+        }
+    }
 })(jQuery);
 
